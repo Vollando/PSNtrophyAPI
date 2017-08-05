cbb8aaebf9e24f69f56e66f5bce6fe0b315c88f4
Vollando <cvb.charles@hotmail.com>
2017-08-05 20:26:30 +0100

    handles multiple requests

--- NodeTutorial/test.js
+++ NodeTutorial/test.js
@@ -1,6 +1,22 @@
-var person = {
-    firstName: "Bucky",
-    lastName: "Roberts",
-    age: 28
+function placeOrder(orderNumber){
+
+    console.log("Customer order:", orderNumber);
+
+    cookAndDeliverFood(function (){
+        console.log("Delivered food order:", orderNumber);
+    })
 }
-console.log(person);
+
+//simulate 5 second operation
+function cookAndDeliverFood(callback){
+    setTimeout(callback, 5000);
+}
+
+// simulation users web request
+placeOrder(1);
+placeOrder(2);
+placeOrder(3);
+placeOrder(4);
+placeOrder(5);
+placeOrder(6);
+
